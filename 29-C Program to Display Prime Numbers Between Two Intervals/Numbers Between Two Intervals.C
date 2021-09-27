b1b87a0cc166b87ce6ffe6d3eb06f200364cbe06
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-28 01:00:03 +0530

    Prime Numbers Between Two Intervals
    Prime Numbers Between Two Intervals

--- 29-C Program to Display Prime Numbers Between Two Intervals/Numbers Between Two Intervals.C
+++ 29-C Program to Display Prime Numbers Between Two Intervals/Numbers Between Two Intervals.C
@@ -1,18 +1,37 @@
 #include <stdio.h>
+
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+   int low, high, i, flag;
+   printf("Enter two numbers(intervals): ");
+   scanf("%d %d", &low, &high);
+   printf("Prime numbers between %d and %d are: ", low, high);
 
-    // Computes quotient
-    quotient = dividend / divisor;
+   // iteration until low is not equal to high
+   while (low < high) {
+      flag = 0;
 
-    // Computes remainder
-    remainder = dividend % divisor;
+      // ignore numbers less than 2
+      if (low <= 1) {
+         ++low;
+         continue;
+      }
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    return 0;
+      // if low is a non-prime number, flag will be 1
+      for (i = 2; i <= low / 2; ++i) {
+
+         if (low % i == 0) {
+            flag = 1;
+            break;
+         }
+      }
+
+      if (flag == 0)
+         printf("%d ", low);
+
+      // to check prime for the next number
+      // increase low by 1
+      ++low;
+   }
+
+   return 0;
 }
