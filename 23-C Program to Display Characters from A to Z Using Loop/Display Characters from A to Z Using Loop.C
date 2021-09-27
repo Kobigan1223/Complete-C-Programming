7b6a6f606de29f5ee44e72f8bfb7d2c69809b193
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-28 00:55:20 +0530

    Alphabets add
     Alphabets

--- 23-C Program to Display Characters from A to Z Using Loop/Display Characters from A to Z Using Loop.C
+++ 23-C Program to Display Characters from A to Z Using Loop/Display Characters from A to Z Using Loop.C
@@ -1,18 +1,7 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
-
-    // Computes quotient
-    quotient = dividend / divisor;
-
-    // Computes remainder
-    remainder = dividend % divisor;
-
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
+    char c;
+    for (c = 'A'; c <= 'Z'; ++c)
+        printf("%c ", c);
     return 0;
 }
