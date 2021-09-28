f269554ea2f1012242c48fb6a1ad529b1cd7e319
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-29 01:01:17 +0530

    New Update
    New Update

--- 44-C program to calculate the power using recursion/calculate the power using recursion.C
+++ 44-C program to calculate the power using recursion/calculate the power using recursion.C
@@ -1,18 +1,19 @@
 #include <stdio.h>
+int power(int n1, int n2);
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
-
-    // Computes quotient
-    quotient = dividend / divisor;
-
-    // Computes remainder
-    remainder = dividend % divisor;
-
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
+    int base, a, result;
+    printf("Enter base number: ");
+    scanf("%d", &base);
+    printf("Enter power number(positive integer): ");
+    scanf("%d", &a);
+    result = power(base, a);
+    printf("%d^%d = %d", base, a, result);
     return 0;
+}
+
+int power(int base, int a) {
+    if (a != 0)
+        return (base * power(base, a - 1));
+    else
+        return 1;
 }
