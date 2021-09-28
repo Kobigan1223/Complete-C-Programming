f53c34c0767d2b3b68a51099268dded0d7cb61c4
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-29 01:03:21 +0530

    Updated
    Updated

--- 45-C Program to Calculate Average Using Arrays/Calculate Average Using Arrays.C
+++ 45-C Program to Calculate Average Using Arrays/Calculate Average Using Arrays.C
@@ -1,18 +1,24 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+    int n, i;
+    float num[100], sum = 0.0, avg;
 
-    // Computes quotient
-    quotient = dividend / divisor;
+    printf("Enter the numbers of elements: ");
+    scanf("%d", &n);
 
-    // Computes remainder
-    remainder = dividend % divisor;
+    while (n > 100 || n < 1) {
+        printf("Error! number should in range of (1 to 100).\n");
+        printf("Enter the number again: ");
+        scanf("%d", &n);
+    }
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
+    for (i = 0; i < n; ++i) {
+        printf("%d. Enter number: ", i + 1);
+        scanf("%f", &num[i]);
+        sum += num[i];
+    }
+
+    avg = sum / n;
+    printf("Average = %.2f", avg);
     return 0;
 }
