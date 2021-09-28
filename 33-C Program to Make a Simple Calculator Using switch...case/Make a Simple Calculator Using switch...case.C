8e2dfa8ea96d999c04d1c9dc9be903bf0d02b70d
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-29 00:50:06 +0530

    Make a Simple Calculator Using switch...case updated
    Make a Simple Calculator Using switch...case

--- 33-C Program to Make a Simple Calculator Using switch...case/Make a Simple Calculator Using switch...case.C
+++ 33-C Program to Make a Simple Calculator Using switch...case/Make a Simple Calculator Using switch...case.C
@@ -1,18 +1,29 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
 
-    // Computes quotient
-    quotient = dividend / divisor;
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
 
-    // Computes remainder
-    remainder = dividend % divisor;
-
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    return 0;
+  return 0;
 }
