bf1412563d34e5fd4a9c56372dfee8b2dcc4b8d2
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-30 01:06:10 +0530

    Call by Reference
    Call by Reference

--- 53-C Program Swap Numbers in Cyclic Order Using Call by Reference/Cyclic Order Using Call by Reference.C
+++ 53-C Program Swap Numbers in Cyclic Order Using Call by Reference/Cyclic Order Using Call by Reference.C
@@ -1,18 +1,27 @@
 #include <stdio.h>
+void cyclicSwap(int *a, int *b, int *c);
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+    int a, b, c;
 
-    // Computes quotient
-    quotient = dividend / divisor;
+    printf("Enter a, b and c respectively: ");
+    scanf("%d %d %d", &a, &b, &c);
 
-    // Computes remainder
-    remainder = dividend % divisor;
+    printf("Value before swapping:\n");
+    printf("a = %d \nb = %d \nc = %d\n", a, b, c);
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
+    cyclicSwap(&a, &b, &c);
+
+    printf("Value after swapping:\n");
+    printf("a = %d \nb = %d \nc = %d", a, b, c);
+
     return 0;
+}
+
+void cyclicSwap(int *n1, int *n2, int *n3) {
+    int temp;
+    // swapping in cyclic order
+    temp = *n2;
+    *n2 = *n1;
+    *n1 = *n3;
+    *n3 = temp;
 }
