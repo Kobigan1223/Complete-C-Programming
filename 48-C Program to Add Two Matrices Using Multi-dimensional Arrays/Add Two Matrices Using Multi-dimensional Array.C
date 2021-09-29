e0f6cf435602425bb39eca4c6be62eadfdb76a45
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-09-30 01:02:28 +0530

    Add Two Matrices
    Add Two Matrices

--- 48-C Program to Add Two Matrices Using Multi-dimensional Arrays/Add Two Matrices Using Multi-dimensional Array.C
+++ 48-C Program to Add Two Matrices Using Multi-dimensional Arrays/Add Two Matrices Using Multi-dimensional Array.C
@@ -1,18 +1,40 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+  printf("Enter the number of rows (between 1 and 100): ");
+  scanf("%d", &r);
+  printf("Enter the number of columns (between 1 and 100): ");
+  scanf("%d", &c);
 
-    // Computes quotient
-    quotient = dividend / divisor;
+  printf("\nEnter elements of 1st matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element a%d%d: ", i + 1, j + 1);
+      scanf("%d", &a[i][j]);
+    }
 
-    // Computes remainder
-    remainder = dividend % divisor;
+  printf("Enter elements of 2nd matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element b%d%d: ", i + 1, j + 1);
+      scanf("%d", &b[i][j]);
+    }
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    return 0;
+  // adding two matrices
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      sum[i][j] = a[i][j] + b[i][j];
+    }
+
+  // printing the result
+  printf("\nSum of two matrices: \n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("%d   ", sum[i][j]);
+      if (j == c - 1) {
+        printf("\n\n");
+      }
+    }
+
+  return 0;
 }
