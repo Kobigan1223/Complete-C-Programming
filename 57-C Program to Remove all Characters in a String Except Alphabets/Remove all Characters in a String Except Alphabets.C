ee4535e70749a8a49a7f30841378f283550df78a
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-01 10:08:04 +0530

    Remove all Characters i

--- 57-C Program to Remove all Characters in a String Except Alphabets/Remove all Characters in a String Except Alphabets.C
+++ 57-C Program to Remove all Characters in a String Except Alphabets/Remove all Characters in a String Except Alphabets.C
@@ -1,18 +1,26 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+   char line[150];
+   
+   printf("Enter a string: ");
+   fgets(line, sizeof(line), stdin); // take input
 
-    // Computes quotient
-    quotient = dividend / divisor;
 
-    // Computes remainder
-    remainder = dividend % divisor;
+   for (int i = 0, j; line[i] != '\0'; ++i) {
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    return 0;
+      // enter the loop if the character is not an alphabet
+      // and not the null character
+      while (!(line[i] >= 'a' && line[i] <= 'z') && !(line[i] >= 'A' && line[i] <= 'Z') && !(line[i] == '\0')) {
+         for (j = i; line[j] != '\0'; ++j) {
+
+            // if jth element of line is not an alphabet,
+            // assign the value of (j+1)th element to the jth element
+            line[j] = line[j + 1];
+         }
+         line[j] = '\0';
+      }
+   }
+   printf("Output String: ");
+   puts(line);
+   return 0;
 }
