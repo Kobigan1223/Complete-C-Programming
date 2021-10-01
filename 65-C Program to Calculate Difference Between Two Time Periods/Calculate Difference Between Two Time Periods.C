e8c93bdabc105d6a81715e727c47934aa8549f0d
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-01 10:15:31 +0530

    Updated Calculate Difference Between Two Time Periods
    Calculate Difference Between Two Time Periods

--- 65-C Program to Calculate Difference Between Two Time Periods/Calculate Difference Between Two Time Periods.C
+++ 65-C Program to Calculate Difference Between Two Time Periods/Calculate Difference Between Two Time Periods.C
@@ -1,18 +1,56 @@
 #include <stdio.h>
+struct TIME {
+   int seconds;
+   int minutes;
+   int hours;
+};
+
+void differenceBetweenTimePeriod(struct TIME t1,
+                                 struct TIME t2,
+                                 struct TIME *diff);
+
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+   struct TIME startTime, stopTime, diff;
 
-    // Computes quotient
-    quotient = dividend / divisor;
+   printf("Enter the start time. \n");
+   printf("Enter hours, minutes and seconds: ");
+   scanf("%d %d %d", &startTime.hours,
+         &startTime.minutes,
+         &startTime.seconds);
 
-    // Computes remainder
-    remainder = dividend % divisor;
+   printf("Enter the stop time. \n");
+   printf("Enter hours, minutes and seconds: ");
+   scanf("%d %d %d", &stopTime.hours,
+         &stopTime.minutes,
+         &stopTime.seconds);
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    return 0;
+   // Difference between start and stop time
+   differenceBetweenTimePeriod(startTime, stopTime, &diff);
+   printf("\nTime Difference: %d:%d:%d - ", startTime.hours,
+          startTime.minutes,
+          startTime.seconds);
+   printf("%d:%d:%d ", stopTime.hours,
+          stopTime.minutes,
+          stopTime.seconds);
+   printf("= %d:%d:%d\n", diff.hours,
+          diff.minutes,
+          diff.seconds);
+   return 0;
+}
+
+// Computes difference between time periods
+void differenceBetweenTimePeriod(struct TIME start,
+                                 struct TIME stop,
+                                 struct TIME *diff) {
+   while (stop.seconds > start.seconds) {
+      --start.minutes;
+      start.seconds += 60;
+   }
+   diff->seconds = start.seconds - stop.seconds;
+   while (stop.minutes > start.minutes) {
+      --start.hours;
+      start.minutes += 60;
+   }
+   diff->minutes = start.minutes - stop.minutes;
+   diff->hours = start.hours - stop.hours;
 }
