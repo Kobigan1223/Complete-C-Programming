adf66dfbcb67ba4e489cb26895f4bf60423a315c
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-01 10:07:22 +0530

    new files added

--- 56-C Program to Count the Number of Vowels, Consonants and so on/Count the Number of Vowels, Consonants and so on.C
+++ 56-C Program to Count the Number of Vowels, Consonants and so on/Count the Number of Vowels, Consonants and so on.C
@@ -1,18 +1,31 @@
 #include <stdio.h>
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+    char line[150];
+    int vowels, consonant, digit, space;
 
-    // Computes quotient
-    quotient = dividend / divisor;
+    vowels = consonant = digit = space = 0;
 
-    // Computes remainder
-    remainder = dividend % divisor;
+    printf("Enter a line of string: ");
+    fgets(line, sizeof(line), stdin);
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
+    for (int i = 0; line[i] != '\0'; ++i) {
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+            line[i] == 'U') {
+            ++vowels;
+        } else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) {
+            ++consonant;
+        } else if (line[i] >= '0' && line[i] <= '9') {
+            ++digit;
+        } else if (line[i] == ' ') {
+            ++space;
+        }
+    }
+
+    printf("Vowels: %d", vowels);
+    printf("\nConsonants: %d", consonant);
+    printf("\nDigits: %d", digit);
+    printf("\nWhite spaces: %d", space);
     return 0;
 }
