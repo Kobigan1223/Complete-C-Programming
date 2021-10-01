238e32fca6259f3c00f2c182124d7ad4573aa604
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-01 10:12:02 +0530

    Lexicographical Order

--- 61-C Program to Sort Elements in Lexicographical Order (Dictionary Order)/Lexicographical Order (Dictionary Order).C
+++ 61-C Program to Sort Elements in Lexicographical Order (Dictionary Order)/Lexicographical Order (Dictionary Order).C
@@ -1,18 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
 int main() {
-    int dividend, divisor, quotient, remainder;
-    printf("Enter dividend: ");
-    scanf("%d", &dividend);
-    printf("Enter divisor: ");
-    scanf("%d", &divisor);
+   char str[5][50], temp[50];
+   printf("Enter 5 words: ");
 
-    // Computes quotient
-    quotient = dividend / divisor;
+   // Getting strings input
+   for (int i = 0; i < 5; ++i) {
+      fgets(str[i], sizeof(str[i]), stdin);
+   }
 
-    // Computes remainder
-    remainder = dividend % divisor;
+   // storing strings in the lexicographical order
+   for (int i = 0; i < 5; ++i) {
+      for (int j = i + 1; j < 5; ++j) {
 
-    printf("Quotient = %d\n", quotient);
-    printf("Remainder = %d", remainder);
-    return 0;
+         // swapping strings if they are not in the lexicographical order
+         if (strcmp(str[i], str[j]) > 0) {
+            strcpy(temp, str[i]);
+            strcpy(str[i], str[j]);
+            strcpy(str[j], temp);
+         }
+      }
+   }
+
+   printf("\nIn the lexicographical order: \n");
+   for (int i = 0; i < 5; ++i) {
+      fputs(str[i], stdout);
+   }
+   return 0;
 }
